5910434ea9046c800c9a0ff4800a309a75e8f886
Kaike Michel De Lima Mota <00021520@fepi.local>
2024-08-29 22:25:29 -0300

    meu primeiro commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const gitHubid = document.querySelector("input[type=text]");
+const btnBuscar = document.querySelector("input[type=button]");
+
+btnBuscar.onclick = function(){
+    const headers = {
+        Authorization: "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"
+    };
+    fetch("https://api.github.com/users/" + gitHubid.value, {headers})
+    .then(response => {
+        if(response.ok){
+            return response.json();
+        }else{
+            if(response.status === 404){
+                alert("Usuário não existe!");
+                throw new Error("404:Usuário não existe!");
+
+                
+            }else {
+                alert("Erro na requisição.");
+                throw new Error("Erro de requisição");
+            }
+        }
+    })
+    .then(data =>{
+        document.querySelector("#name").textContent = data.name;
+        document.querySelector("#bio").textContent = data.bio;
+        document.querySelector("#followers").textContent = data.followers;
+        document.querySelector("#location").textContent = data.location;
+        document.querySelector("#avatar").src = data.avatar_url;
+    })
+    .catch (error => {
+        console.log(error);
+    })
+ }
